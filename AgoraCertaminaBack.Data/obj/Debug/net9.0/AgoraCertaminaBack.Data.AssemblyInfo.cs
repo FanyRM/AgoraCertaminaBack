85e4ef539a501127103d8f91ed2cf8e003d1dba1
FanyRM <celeste.e.ramirez.m@gmail.com>
2025-10-19 15:10:10 -0600

    Se agrega ROP para
    Añadir paquete Netmentor.ROP y actualizar versiones
    
    Se ha añadido la referencia al paquete `Netmentor.ROP` en `AgoraCertaminaBack.UseCases.csproj` (versión `1.12.0`).
    Se han actualizado las dependencias en `project.assets.json` y las versiones de `AssemblyInformationalVersion` en `AssemblyInfo.cs` y `AssemblyInfoInputs.cache`.
    También se han realizado cambios en varios archivos de caché y especificaciones de NuGet para incluir las nuevas dependencias y sus metadatos.

--- AgoraCertaminaBack.Data/obj/Debug/net9.0/AgoraCertaminaBack.Data.AssemblyInfo.cs
+++ AgoraCertaminaBack.Data/obj/Debug/net9.0/AgoraCertaminaBack.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AgoraCertaminaBack.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56ac586be0f178725e86d614670c3820be15a0ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5963fe6c86836a8c3fc29eac25c555d87cd3968b")]
 [assembly: System.Reflection.AssemblyProductAttribute("AgoraCertaminaBack.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AgoraCertaminaBack.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
